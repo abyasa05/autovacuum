2f7fa04aaffc1939d429bee61a137a3d09011443
abyasa05 <abyprat07@gmail.com>
2026-09-08 08:23:44 +0700

    refactor: move css file to root dir

--- config/settings.py
+++ config/settings.py
@@ -99,6 +99,9 @@ PASSWORD_HASHERS = [
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
